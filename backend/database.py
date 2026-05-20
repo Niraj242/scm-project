87884c2d216d54fbf328ef89b07cc22e5995beb1
Niraj242 <nirajhkurane@gmail.com>
2026-05-20 23:11:11 +0530

    Fix database and order backend config for deployment

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,11 @@
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from sqlalchemy import create_engine
 
-DATABASE_URL = "postgresql://neondb_owner:npg_EeyF0hi3JRcV@ep-wild-resonance-aoturwvf.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+DATABASE_URL = os.getenv("postgresql://neondb_owner:npg_EeyF0hi3JRcV@ep-wild-resonance-aoturwvf.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require")
+
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL not set")
 
 engine = create_engine(
     DATABASE_URL,

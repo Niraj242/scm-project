0ecbd9775d07bd73da508bdac71d4790b6709cc9
Niraj242 <nirajhkurane@gmail.com>
2026-06-11 13:48:59 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,8 @@ from schemas import RegisterRequest, LoginRequest
 from order_backend_postgres import router as order_router
 from traceability_backend import router as traceability_router
 from tbe_backend import router as tbe_router  # New TBE Module Import
-
+from scrap_backend import router as scrap_router
+from afterchannel_backend import router as afterchannel_router
 
 
 # ================= APP =================
@@ -98,8 +99,8 @@ def delete_user(user_id: int, db: Session = Depends(get_db)):
 app.include_router(order_router)
 app.include_router(traceability_router)
 app.include_router(tbe_router)  # Registered TBE Module Router
-
-
+app.include_router(scrap_router)
+app.include_router(afterchannel_router)
 
 # ================= BASE ROOT CONTROLLER =================
 @app.get("/")
